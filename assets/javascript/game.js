9bd8161aaba18bdffb5c35535c453cb2bc7ac165
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-03-08 17:18:44 -0700

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,79 @@
 $(document).ready(() => {
-  
-  
-
- $('#apollo').data({"name":'Apollo',"isVillian":true,"attackpower":0, "healthscore": 0,"startingHealth":120, "soundfile":"" });
- 
- $('#starbuck').data({"name":'Star Buck',"isVillian":true,"attackpower":0, "healthscore": 0,"startingHealth":100, "soundfile":"" });
-
- $('#cylone').data({"name":'Cylone',"isVillian":true,"attackpower":0, "healthscore": 0,"startingHealth":150, "soundfile":"" });
- 
- $('#numberSix').data({"name":"NumberSix","isVillian":true,"attackpower":0, "healthscore": 0,"startingHealth":180, "soundfile":"" });
-
-
-
-
-
-//Choose Your Fighter
-
-$('.port').on('click', event => {
-    $(event.currentTarget).css('border','5px solid red')
-     console.log(event.currentTarget);
-       
-     const $object = $(event.currentTarget);
-     console.log($('#apollo').data("isVillian"));
-     $object.data({"startingHealth":25});
-      
-      
-       console.log($('#apollo').data("startingHealth"));
-       console.log($('#numberSix').data("startingHealth"))
+  $("#apollo").data({
+    name: "Apollo",
+    isVillian: true,
+    attackpower: 0,
+    healthscore: 0,
+    startingHealth: 120,
+    soundfile: ""
   });
 
+  $("#starbuck").data({
+    name: "Star Buck",
+    isVillian: true,
+    attackpower: 0,
+    healthscore: 0,
+    startingHealth: 100,
+    soundfile: ""
+  });
 
+  $("#cylone").data({
+    name: "Cylone",
+    isVillian: true,
+    attackpower: 0,
+    healthscore: 0,
+    startingHealth: 150,
+    soundfile: ""
+  });
 
+  $("#numberSix").data({
+    name: "NumberSix",
+    isVillian: true,
+    attackpower: 0,
+    healthscore: 0,
+    startingHealth: 180,
+    soundfile: ""
+  });
 
+  //Choose Your Fighter
 
+  $(".port").on("click", event => {
+    if ($(event.currentTarget.style.border == "")) {
+      $(event.currentTarget).css("border", "5px solid yellow");
+      console.log(event.currentTarget);
+
+      const $object = $(event.currentTarget);
+      $object.data({ isVillian: false });
+
+      // $('#starbuck').clone().appendTo($('#r2c2'));
+      // $('#starbuck').remove();
+      var r2c2 = $("#r2c2");
+      var r2c3 = $("#r2c3");
+      var r2c4 = $("#r2c4");
+
+      var rowArr = [r2c2, r2c3, r2c4];
+      var mycol = 0;
+      $(".port").each(function() {
+        console.log($(this.style.border));
+
+        if (this.style.border == "") {
+          this.style.border = "5px solid red";
+          console.log(rowArr[0]);
+          // $(this).clone().appendTo($('#r2c2'));
+
+          $(this)
+            .clone()
+            .appendTo(rowArr[mycol]);
+          $(this).remove();
+          mycol++;
+        }
+      });
+    } else {
+      console.log(event.currentTarget);
+      $(event.currentTarget).clone().appendTo($("#defender"));
+      $(event.currentTarget).remove();
+    } 
+  });
 
 
 
